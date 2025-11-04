1788e0dc20e83aa693eabf19a42001b28c99d653
Aansh Malhotra <aanshmalhotra49@gmail.com>
2025-11-04 15:01:41 +0530

    updated image error in deployment

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
         <header className="fixed w-full bg-white z-50 shadow-sm">
         <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-        <img src="../public/digiQ.jpg" alt="Logo" className="h-10 w-auto" />
+        <img src="/digiQ.jpg" alt="Logo" className="h-10 w-auto" />
           <nav className="space-x-5 text-gray-700 font-medium">
             {/* <a href="#features" className="hover:text-blue-600 transition">Features</a>
             <a href="#how-it-works" className="hover:text-blue-600 transition">How It Works</a>
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
-const allowedOrigins = ["https://digi-q-ivory.vercel.app"]
+const allowedOrigins = ["https://digi-q-ivory.vercel.app" ,"http://localhost:5173" ]
 app.use(cors({credentials: true , origin: allowedOrigins}))
 
 app.use("/api/auth/user" , userRouter)
